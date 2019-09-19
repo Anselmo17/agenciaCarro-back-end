85db2bc6e75c4095336ea968708e8cf40216c70a
Anselmo <anselmo.dos.santos@avanade.com>
2019-09-18 21:03:02 -0300

    Config db

--- models/index.js
+++ models/index.js
@@ -6,26 +6,31 @@ const mysql = require('mysql');
 
 const config = require('../config/database').db;
 
-// teste de conexao 
-// connection.connect(function(err){
-//   if(err) return console.log('Houve um erro : ', err);
-//   console.log('Conectado ao DATABASE MYSQL');
-// })
 
-
-logger.debug('Connecting to database Mysql', {
+logger.debug('Config Database Mysql : ', {
   host: config.host,
   user: config.username,
   database: config.database,
 });
 
-const sequelize = new Sequelize(config.database, config.username, config.password, config);
+const connect = new Sequelize(config.database, config.username, config.password, config);
+
+// teste de conexao ao banco de dados 
+connect.authenticate()
+  .then(function () {
+    logger.debug('Conectado com sucesso ao Mysql')
+  })
+  .catch(function (){
+    logger.debug('Não foi possível conectar ao banco de dados Mysql')
+  });
+
+
 const db = {};
 
 fs.readdirSync(__dirname)
   .filter(file => (file.indexOf('.') !== 0) && (file !== 'index.js'))
   .forEach((file) => {
-    const model = sequelize.import(path.join(__dirname, file));
+    const model = connect.import(path.join(__dirname, file));
     db[model.name] = model;
   });
 
